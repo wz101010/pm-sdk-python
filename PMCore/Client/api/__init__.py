ba50fb417ff19da231f6ccaf023fcf6c731fba9b
wangzhen <wangzhen91510@foxmail.com>
2025-03-12 17:04:56 +0800

    update

---
+++ PMCore/Client/api/__init__.py
@@ -0,0 +1,25 @@
+# flake8: noqa
+
+# import apis into api package
+from PMCore.Client.api.access_token_api import AccessTokenApi
+from PMCore.Client.api.alipay_api import AlipayApi
+from PMCore.Client.api.app_api import AppApi
+from PMCore.Client.api.app_setting_api import AppSettingApi
+from PMCore.Client.api.authorize_policy_api import AuthorizePolicyApi
+from PMCore.Client.api.currency_api import CurrencyApi
+from PMCore.Client.api.ding_talk_api import DingTalkApi
+from PMCore.Client.api.file_api import FileApi
+from PMCore.Client.api.o_auth_api import OAuthApi
+from PMCore.Client.api.order_api import OrderApi
+from PMCore.Client.api.project_api import ProjectApi
+from PMCore.Client.api.service_setting_api import ServiceSettingApi
+from PMCore.Client.api.storage_api import StorageApi
+from PMCore.Client.api.system_api import SystemApi
+from PMCore.Client.api.team_api import TeamApi
+from PMCore.Client.api.tenant_api import TenantApi
+from PMCore.Client.api.tenant_file_api import TenantFileApi
+from PMCore.Client.api.user_api import UserApi
+from PMCore.Client.api.user_currency_api import UserCurrencyApi
+from PMCore.Client.api.user_setting_api import UserSettingApi
+from PMCore.Client.api.wechat_api import WechatApi
+
